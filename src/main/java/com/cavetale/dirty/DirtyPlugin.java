e78e9249db043b1f20122575ceddbafd3d094b36
StarTux <ths.loy@googlemail.com>
2018-08-26 13:33:32 +0200

    Update for 1.13.1

--- src/main/java/com/cavetale/dirty/DirtyPlugin.java
+++ src/main/java/com/cavetale/dirty/DirtyPlugin.java
@@ -3,18 +3,16 @@ package com.cavetale.dirty;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
+@Plugin(name = "Dirty", version = "0.1")
+@Description("Quick and dirty access to NBT and CBS code")
+@ApiVersion(ApiVersion.Target.v1_13)
+@Author("StarTux")
+@Website("https://cavetale.com")
 public final class DirtyPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-    }
-
-    @Override
-    public void onDisable() {
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-        return true;
-    }
 }
